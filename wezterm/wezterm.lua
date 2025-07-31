421961b916f1f893b59f93625002152956612a30
Lee, DaeIn <lazy0ren@gmail.com>
2025-07-31 14:59:03 +0900

    remove comments

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,9 +2,6 @@ local Config = require("config")
 
 require("events.workspaceSwitcher")
 require("events.status").setup()
--- require('events.left-status').setup()
--- require('events.right-status').setup({ date_format = '%a %H:%M:%S' })
--- require('events.tab-title').setup({ hide_active_tab_unseen = false, unseen_icon = 'circle' })
 
 return Config:init()
     :append(require("config.appearance"))
