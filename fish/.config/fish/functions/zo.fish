7306e92d1f35ed9597abf249f4d6bc04d403558b
Lee, DaeIn <lazy0ren@gmail.com>
2026-03-30 18:05:04 +0900

    Allow 'zo' command to take argument

--- fish/.config/fish/functions/zo.fish
+++ fish/.config/fish/functions/zo.fish
@@ -1,3 +1,6 @@
 function zo --description "Open zellij session named after current directory"
+    if set -q argv[1]
+        z $argv[1]; or return 1
+    end
     zellij attach --create (basename $PWD)
 end
