01d96abd24ae011d38db583cea71c98f92c3468e
Lee, DaeIn <lazy0ren@gmail.com>
2026-04-02 02:33:29 +0900

    Change file name

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    opts = {
+      style = "darker",
+      transparent = true,
+      lualine = { transparent = false },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
