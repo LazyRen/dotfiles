1698bb5f01df34c6cdeac826f9bb38f59706a6f7
Lee, DaeIn <lazy0ren@gmail.com>
2026-04-02 13:36:55 +0900

    Remove cmd+esc default keybidning of mac

--- os/mac/setup.sh
+++ os/mac/setup.sh
@@ -25,6 +25,8 @@ defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock mru-spaces -bool false
 # Group windows by application in Mission Control
 defaults write com.apple.dock expose-group-apps -bool true
+# Disable Game Overlay shortcut (Cmd+Esc, hotkey ID 24)
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 24 '<dict><key>enabled</key><false/></dict>'
 
 # --- Language & Region ---
 # Set English as primary language with Korean region
@@ -65,8 +67,9 @@ defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 61 \
   '<dict><key>enabled</key><true/><key>value</key><dict><key>parameters</key><array><integer>65535</integer><integer>105</integer><integer>8388608</integer></array><key>type</key><string>standard</string></dict></dict>'
 
 # --- Keyboard > Shortcuts > Spotlight ---
-# Disable Spotlight shortcut (use Alfred instead)
+# Disable Show Spotlight search (Cmd+Space, hotkey ID 64)
 defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 64 '<dict><key>enabled</key><false/></dict>'
+# Disable Show Finder search window (Cmd+Option+Space, hotkey ID 65)
 defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 65 '<dict><key>enabled</key><false/></dict>'
 
 # --- Trackpad > Point & Click ---
