1a366ed4519df3072c3f6a6857fd27b664f42ab4
Lee, DaeIn <daein.lee@sap.com>
2026-04-03 21:42:53 +0000

    noice popup on center

---
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    presets = {
+      bottom_search = false,
+    },
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "50%",
+          col = "50%",
+        },
+      },
+    },
+  },
+}
