e65a2fbdd28a38637cc4c59abd34b52406afff94
Lee, DaeIn <lazy0ren@gmail.com>
2026-04-03 19:00:16 +0900

    Allow 'already in the only match' error

--- fish/.config/fish/functions/zo.fish
+++ fish/.config/fish/functions/zo.fish
@@ -1,6 +1,14 @@
 function zo --description "Open zellij session named after current directory"
     if set -q argv[1]
-        z $argv[1]; or return 1
+        set -l z_output (z $argv[1] 2>&1)
+        if test $status -ne 0
+            if string match -q '*already in the only match*' "$z_output"
+                # already in the target directory, continue
+            else
+                echo $z_output >&2
+                return 1
+            end
+        end
     end
     zellij attach --create (basename $PWD)
 end
