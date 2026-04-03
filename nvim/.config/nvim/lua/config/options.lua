0633e9bd1ace4a7280600938db83014e76af8b3b
Lee, DaeIn <daein.lee@sap.com>
2026-04-03 21:42:33 +0000

    Copy to clipboard on ssh yank

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -9,3 +9,19 @@ o.smarttab = true
 o.autoindent = true
 o.copyindent = true
 o.breakindent = true
+
+if os.getenv("SSH_TTY") then
+  -- LazyVim disables clipboard over SSH; re-enable with OSC52 provider
+  o.clipboard = "unnamedplus"
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
