383dbe2d0ebb0d13ede379de49a1643f3e0fe54f
Lee, DaeIn <lazy0ren@gmail.com>
2026-04-06 11:15:41 +0900

    Add flash support for picker

--- nvim/.config/nvim/lua/plugins/snacks-picker.lua
+++ nvim/.config/nvim/lua/plugins/snacks-picker.lua
@@ -1,12 +1,58 @@
+local flash_on_picker = function(picker)
+  require("flash").jump({
+    pattern = "^",
+    label = { after = { 0, 0 } },
+    search = {
+      mode = "search",
+      exclude = {
+        function(win)
+          return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
+        end,
+      },
+    },
+    action = function(match)
+      local idx = picker.list:row2idx(match.pos[1])
+      picker.list:_move(idx, true, true)
+    end,
+  })
+end
+
 return {
   "folke/snacks.nvim",
   opts = {
     picker = {
+      actions = {
+        flash = flash_on_picker,
+      },
       sources = {
         files = {
           hidden = true,
         },
       },
+      win = {
+        input = {
+          keys = {
+            ["<a-s>"] = { "flash", mode = { "n", "i" } },
+            ["s"] = { "flash" },
+          },
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>b",
+      function()
+        Snacks.picker.buffers({
+          on_show = function(picker)
+            vim.cmd.stopinsert()
+            vim.schedule(function()
+              flash_on_picker(picker)
+            end)
+          end,
+        })
+      end,
+      desc = "Buffers",
     },
   },
 }
