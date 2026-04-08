226643943ec89d344f88bd29ec11c883fab1ba97
Lee, DaeIn <daein.lee@sap.com>
2026-04-08 01:46:48 +0000

    Only show cwd for recent files

--- nvim/.config/nvim/lua/plugins/snacks-dashboard.lua
+++ nvim/.config/nvim/lua/plugins/snacks-dashboard.lua
@@ -68,22 +68,8 @@ return {
             },
             align = "left",
           },
-          { pane = 2, icon = "   ", title = "Recent Files", section = "recent_files", indent = 4, padding = 4 },
+          { pane = 2, icon = "   ", title = "Recent Files", section = "recent_files", cwd = true, indent = 4, padding = 4 },
           { pane = 2, icon = "   ", title = "Projects",     section = "projects",     indent = 4, padding = 4 },
-          {
-            pane = 2,
-            icon = "   ",
-            title = "Git Status",
-            section = "terminal",
-            enabled = function()
-              return require('snacks.git').get_root() ~= nil
-            end,
-            cmd = "git status --short --branch --renames",
-            height = 5,
-            padding = 4,
-            ttl = 5 * 60,
-            indent = 4,
-          },
           { section = "startup" },
         },
       },
