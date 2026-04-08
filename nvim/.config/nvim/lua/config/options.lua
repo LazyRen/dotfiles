986cbcb1b80781e9a67f4c91a38d5ea8a3bd6ede
Lee, DaeIn <daein.lee@sap.com>
2026-04-08 00:58:00 +0000

    Set scrolloffset to 16

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,24 +3,30 @@ local o = vim.o
 
 vim.g.autoformat = false
 
+-- Indentation
 o.shiftwidth = 4
 o.tabstop = 4
 o.softtabstop = 4
-
 o.smarttab = true
 o.autoindent = true
 o.copyindent = true
 o.breakindent = true
+
+-- Scrolling
+o.scrolloff = 16
+
+-- Spelling
 o.spelloptions = "camel"
 
-local function my_paste()
-  return function()
-    local content = vim.fn.getreg('"')
-    return vim.split(content, "\n")
-  end
-end
-
+-- Clipboard (SSH)
 if os.getenv("SSH_TTY") then
+  local function myPaste()
+    return function()
+      local content = vim.fn.getreg('"')
+      return vim.split(content, "\n")
+    end
+  end
+
   -- LazyVim disables clipboard over SSH; re-enable with OSC52 provider
   o.clipboard = "unnamedplus"
   vim.g.clipboard = {
@@ -30,8 +36,8 @@ if os.getenv("SSH_TTY") then
       ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
     },
     paste = {
-      ["+"] = my_paste(),
-      ["*"] = my_paste(),
+      ["+"] = myPaste(),
+      ["*"] = myPaste(),
     },
   }
 end
