c3f29beef6692d62e30417ae015d2f2adec5d2d0
Lee, DaeIn <lazy0ren@gmail.com>
2026-04-12 01:22:23 +0900

    Adding winbar

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.options.component_separators = ""
+      opts.options.section_separators = { left = "", right = "" }
+      opts.options.disabled_filetypes.winbar = opts.options.disabled_filetypes.statusline
+
+      table.insert(opts.sections.lualine_x, {
+        function()
+          local clients = vim.lsp.get_clients({ bufnr = 0 })
+          if #clients == 0 then return "" end
+          local names = {}
+          for _, c in ipairs(clients) do
+            table.insert(names, c.name)
+          end
+          return table.concat(names, ", ")
+        end,
+        icon = " ",
+        color = { fg = require("onedark.palette").cyan },
+      })
+
+      opts.sections.lualine_z = {
+        function()
+          return os.date("%R")
+        end
+      }
+
+      -- move navic from statusline to winbar
+      local navic = table.remove(opts.sections.lualine_c)
+      opts.winbar = { lualine_b = { "filename" }, lualine_c = { navic } }
+      opts.inactive_winbar = { lualine_b = { "filename" } }
+    end,
+  },
+}
