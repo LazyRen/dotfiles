f5b039063f228a68f26da4dd65a7614d3a09e587
Lee, DaeIn <daein.lee@sap.com>
2026-04-11 16:43:09 +0000

    Add goto-preview

---
+++ nvim/.config/nvim/lua/plugins/goto-preview.lua
@@ -0,0 +1,11 @@
+return {
+  "rmagatti/goto-preview",
+  dependencies = { "rmagatti/logger.nvim" },
+  event = "BufEnter",
+  opts = {
+    default_mappings = true,
+    references = {
+      provider = "snacks",
+    },
+  },
+}
