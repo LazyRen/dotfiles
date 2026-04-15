fefac0d31dab05dae33c1d49848df1fe5d34a832
Lee, DaeIn <lazy0ren@gmail.com>
2026-04-15 14:07:50 +0900

    Fix tab rename

--- fish/.config/fish/conf.d/zellij.fish
+++ fish/.config/fish/conf.d/zellij.fish
@@ -1,19 +1,24 @@
-function zellij_tab_name_update --on-variable PWD --on-event fish_postexec
-    set -q ZELLIJ; or return
-
-    set -l tab_name
+function __zellij_tab_dir_name
     if test "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = true
         set -l root (basename (git rev-parse --show-toplevel))
         set -l prefix (string trim -c / (git rev-parse --show-prefix))
-        set tab_name $root
-        test -n "$prefix"; and set tab_name "$root/$prefix"
+        if test -n "$prefix"
+            echo "$root/$prefix"
+        else
+            echo $root
+        end
     else if test "$PWD" = "$HOME"
-        set tab_name "~"
+        echo "~"
     else
-        set tab_name (basename $PWD)
+        basename $PWD
     end
+end
 
-    command zellij action rename-tab $tab_name &>/dev/null &
+function zellij_tab_name_update --on-variable PWD
+    set -q ZELLIJ; or return
+
+    set -g __zellij_cmd_tab ""
+    command zellij action rename-tab (__zellij_tab_dir_name) &>/dev/null &
     disown
 end
 
@@ -21,9 +26,21 @@ function zellij_tab_name_update_preexec --on-event fish_preexec
     set -q ZELLIJ; or return
 
     set -l title (string split ' ' $argv)[1]
+    set -g __zellij_cmd_tab $title
     command zellij action rename-tab $title &>/dev/null &
     disown
 end
 
+function zellij_tab_name_update_postexec --on-event fish_postexec
+    set -q ZELLIJ; or return
+
+    set -g __zellij_cmd_tab ""
+    command zellij action rename-tab (__zellij_tab_dir_name) &>/dev/null &
+    disown
+end
+
 # Set tab name on shell startup
-zellij_tab_name_update
+if set -q ZELLIJ
+    command zellij action rename-tab (__zellij_tab_dir_name) &>/dev/null &
+    disown
+end
