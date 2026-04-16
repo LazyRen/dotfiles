983920d4abfa752028ed5dd52d2fed58bd28e738
Lee, DaeIn <lazy0ren@gmail.com>
2026-04-17 00:44:52 +0900

    Disable suggestion for plain text and markdown

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,6 +1,12 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    sources = {
+      per_filetype = {
+        text = {},
+        markdown = {},
+      },
+    },
     keymap = {
       ["<Up>"] = { "select_prev", "fallback" },
       ["<Down>"] = { "select_next", "fallback" },
