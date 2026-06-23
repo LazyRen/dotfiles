adad2abe0f670d955e0dcb580f9353dd050b0e78
Lee, DaeIn <lazy0ren@gmail.com>
2026-06-23 14:00:07 +0900

    Fix zo failure to open folder

--- fish/.config/fish/functions/zo.fish
+++ fish/.config/fish/functions/zo.fish
@@ -1,11 +1,11 @@
 function zo --description "Open zellij session named after current directory"
     if set -q argv[1]
         set -l result (command zoxide query -- $argv 2>/dev/null)
-        if test $status -ne 0
+        or begin
             echo "zoxide: no match for '$argv'" >&2
             return 1
         end
-        builtin cd $result
+        builtin cd $result; or return 1
     end
     zellij attach --create (basename $PWD)
 end
