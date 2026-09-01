d682588a346c748c51abf62366a094e7fccdc793
Lee, Daein <lazy0ren@gmail.com>
2026-09-02 01:20:21 +0900

    Update yazi

--- yazi/setup.sh
+++ yazi/setup.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 # Install packages, or upgrade if already installed
-ya pkg install 2>/dev/null || ya pkg upgrade
+ya pkg install --discard 2>/dev/null || ya pkg upgrade --discard
